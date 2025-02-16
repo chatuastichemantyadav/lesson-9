f7cf01c325fce9c0c8b95bb1a6bffdc76c7db83d
chartastichemantyadav <hemantyadav0727@gmail.com>
2025-02-16 12:08:42 +0530

    lol

---
+++ actual.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+a, b, c = c, a, b
+
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
+print("Third number:", c)
